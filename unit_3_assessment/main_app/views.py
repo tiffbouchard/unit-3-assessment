aab5f57653893bff06d088c1218ca141be7cbb32
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-07-11 12:15:05 -0400

    add index view

--- unit_3_assessment/main_app/views.py
+++ unit_3_assessment/main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.edit import DeleteView
+from .models import Widgets
+from .forms import WidgetsForm
 
 # Create your views here.
+def index(request):
+    form = WidgetsForm(request.POST)
+    if form.is_valid():
+        new_widget = form.save(commit=False)
+    return render(request, 'index.html', {
+        'form': form,
+        
+    })
